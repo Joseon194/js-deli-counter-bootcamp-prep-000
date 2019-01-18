1ce84846b7aaabcf669d039699b64c470977a048
Dennis Chung <chungdennis6@gmail.com>
2019-01-18 05:47:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var katzDeli = []; // At the beginning of the day,
-//the deli is empty and is represented by an empty array
-//<<<<<<<<<--------------- in the instructions <<<<<<
+var katzDeli = []; 
 
 function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
@@ -39,27 +37,3 @@ function currentLine(katzDeli) {
 }
 
 
-/// did it this way first, then refactored to above
-//(i used a for loop instead of a while loop)
-
-// function currentLine(katzDeli) {
-//   let i = 0;
-//   while (i < katzDeli.length) {
-//     line.push(` `+[i+1]+`. `  + katzDeli[i])
-//     i++;
-//   }
-//   if (katzDeli.length === 0) {
-//     return "The line is currently empty.";
-//   } else
-//   return(`The line is currently:` + line);
-// }
-
-//WORKS:::::
-
-// function currentLine(katzDeli) {
-//
-//   for (let i = 0; i < katzDeli.length; i++) {
-//     line.push(` `+[i+1]+`. `  + katzDeli[i])
-//   }
-//   return(`The line is currently:` + line);
-// }
